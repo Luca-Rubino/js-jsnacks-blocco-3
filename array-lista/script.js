44abfd23538faa9b51f79145843f1c32a78a2375
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-17 19:28:21 +0200

    Correzione e svolgimento dell'esercizio

--- array-lista/script.js
+++ array-lista/script.js
@@ -15,30 +15,26 @@ let frutta = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'coc
 console.log(frutta)
 //     2. aggiungere la pesca all'array della frutta
 frutta.push('pesca')
+// stampo array frutta per controllo
+console.log(frutta) 
 //     3. verificare se nell'array di frutta c'è il cocomero:
 //     - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
-//     - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!
-
-let i, trovato, fruttaDaCercare
-
-fruttaDaCercare = 'cocomero'
-trovato = false
-
-for (i=0;i<frutta.length;i++){
-    if (frutta[i]==fruttaDaCercare){
-        trovato = true
-    } else {
-        trovato = false
+//     - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+// creo variabile negativa per la ricerca
+let fruttaCercata = 'Oh no, devo uscire a comprare il cocomero!'
+//  creo funzione per cercare elemento cocomero
+for(let i=0; i<frutta.length; i++){
+    // stampo singoli elementi dell'arrey frutta per controllare la corretta scrittura della funzione
+    console.log(frutta[i])
+    // stabilisco condizioni della ricerca andata a buon fine
+    if(frutta[i]=='cocomero'){
+        fruttaCercata ='Trovato! Devo solo preparare il cocktail.'
     }   
 }
-console.log(trovato)
-
-if (trovato!=false){
-    console.log('Oh no, devo uscire a comprare il cocomero!')
-} else {
-    console.log('Trovato! Devo solo preparare il cocktail.') 
-}  
-
+// stampo risultato ricerca
+console.log(fruttaCercata)
+// stampo elenco per ulteriore controllo
+console.log(frutta)
 // "Suggerimenti/Indicazioni:
 //     Sì, lo sappiamo che esistono le funzioni includes() e indexOf() ma noi non le vogliamo utilizzare per cercare nel frigorifero.
 
