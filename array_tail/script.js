e0f2663a75570dd3c54afa5ba0101954cb6b8a22
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-19 11:54:03 +0200

    Svolgimento esercizio array_tail e aggiustamento struttura testo del file readme

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,1 +1,14 @@
 console.log('array_tail')
+// creo array vuoto che conterra un X numero di elementi(che saranno numeri randomici) scelti dall'utente
+const numberList=[]
+// chiedo all'utente quanti elementi deve contenere l'array
+const userNumberElement = Number.parseInt(prompt("Quanti elementi deve avere l'array"))
+
+let i
+// creo funzione per generare nell'array il numero di elementi random richiesti dall'utente
+for(i = 0; i < userNumberElement; i++){
+    console.log(userNumberElement)
+    numberList.push(parseInt(Math.random()*100+1))
+}
+// stampo in console gli ultimi 5 elementi dell'array
+console.log(numberList.slice(-5))
