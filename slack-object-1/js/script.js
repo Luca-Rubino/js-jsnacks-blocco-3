06574f0d2dad28c89aa864bcb3e3eaac0e0c3771
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-23 12:15:16 +0200

    Scrivere traccia, creare array automobili con oggetti

--- slack-object-1/js/script.js
+++ slack-object-1/js/script.js
@@ -1,1 +1,86 @@
 console.log('Slack-object-1')
+
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+const automobili = [
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'metano'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto 2002',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'Bmv',
+        modello:'Serie 2 Active Tourer',
+        alimentazione:'disel'
+    },
+    {
+        marca:'bmv',
+        modello:'X3',
+        alimentazione:'gpl'
+    },
+    {
+        marca:'bmv',
+        modello:'i7.',
+        alimentazione:'eletrico'
+    },
+    {
+        marca:'bmv',
+        modello:'i4 M50',
+        alimentazione:'eletrico'
+    },
+    {
+        marca:'bmv',
+        modello:'Serie 2 Coupé',
+        alimentazione:'gpl'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'disel'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'disel'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'eletrico'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'metano'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'eletrico'
+    },
+    {
+        marca:'Fiat',
+        modello:'Punto evo',
+        alimentazione:'gpl'
+    },
+];
+
+console.log(automobili)
