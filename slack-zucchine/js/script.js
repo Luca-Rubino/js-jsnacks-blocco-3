e259862e24f048e5eafdde0f4d189c8828931808
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-24 11:23:27 +0200

    Creare cartella slack zucchine e creo array con oggetti

---
+++ slack-zucchine/js/script.js
@@ -0,0 +1,60 @@
+console.log('Hi!')
+
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+// Dividi in due array separati le zucchine che misurano meno o più di 8cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+const zucchine = [
+    {
+        varieta:'africana',
+        peso:'150g',
+        lunghezza:'27cm',
+    },
+    {
+        varieta:'tailandese',
+        peso:'80g',
+        lunghezza:'16cm',
+    },
+    {
+        varieta:'filippina',
+        peso:'60g',
+        lunghezza:'15cm',
+    },
+    {
+        varieta:'russa',
+        peso:'45g',
+        lunghezza:'7cm',
+    },
+    {
+        varieta:'norvegese',
+        peso:'100g',
+        lunghezza:'16cm',
+    },
+    {
+        varieta:'canadese',
+        peso:'85g',
+        lunghezza:'19cm',
+    },
+    {
+        varieta:'brasiliana',
+        peso:'158g',
+        lunghezza:'31cm',
+    },
+    {
+        varieta:'a-cavolfiore',
+        peso:'45g',
+        lunghezza:'7cm',
+    },
+    {
+        varieta:'francese',
+        peso:'54g',
+        lunghezza:'10cm',
+    },
+    {
+        varieta:'italiana',
+        peso:'87g',
+        lunghezza:'18cm',
+    }
+]
